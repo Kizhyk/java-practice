469ff12923dc6388bb44360f3aacb5169f73c0f1
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-03 18:27:27 +0300

    task: done Task01 in array

--- src/com/rakovets/course/practice/array/Task01.java
+++ src/com/rakovets/course/practice/array/Task01.java
@@ -5,7 +5,7 @@ import com.rakovets.course.util.StandardInputTask;
 import java.util.Arrays;
 
 /**
- * Разработать программу для электронного дневника:
+ * Разработать программу для электронного дневника,
  * которая работает с отметками только по одному предмету
  *
  * @author Dmitry Rakovets
@@ -25,6 +25,7 @@ public class Task01 extends StandardInputTask {
 
 	/**
 	 * Возвращает средне арифметическую отметку с округлением до 2 знаков.
+	 *
 	 * @param marks отметки
 	 * @return средняя арифметическая отметка
 	 */
@@ -32,11 +33,19 @@ public class Task01 extends StandardInputTask {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используя параметры метода
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return 0.0;
+		double averageMark = 0;
+		int markSum = 0;
+		for (int n : marks) {
+			markSum += n;
+		}
+		averageMark = (double) markSum / marks.length;
+		averageMark = Math.round(averageMark * 100.0) / 100.0;
+		return averageMark;
 	}
 
 	/**
 	 * Возвращает минимальную отметку.
+	 *
 	 * @param marks отметки
 	 * @return минимальная отметка
 	 */
@@ -44,11 +53,13 @@ public class Task01 extends StandardInputTask {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используя параметры метода
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return 0;
+		Arrays.sort(marks);
+		return marks[0];
 	}
 
 	/**
 	 * Возвращает максимальну отметку.
+	 *
 	 * @param marks отметки
 	 * @return максимальная отметка
 	 */
@@ -56,7 +67,8 @@ public class Task01 extends StandardInputTask {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используя параметры метода
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return 0;
+		Arrays.sort(marks);
+		return marks[marks.length - 1];
 	}
 
 	private static int[] nextArray(int countMarks) {
