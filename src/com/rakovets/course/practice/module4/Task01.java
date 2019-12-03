9b78a3017d228b3701187a9a2519ade314425def
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-03 18:41:33 +0300

    task: done Task01 in loop

--- src/com/rakovets/course/practice/module4/Task01.java
+++ src/com/rakovets/course/practice/module4/Task01.java
@@ -3,13 +3,13 @@ package com.rakovets.course.practice.module4;
 import java.util.Scanner;
 
 /**
- * Разработать программу для табличного процессора:
+ * Разработать программу для табличного процессора.
  * Программа генерирует номера строк при создании таблицы. Известно:
- * @param rows количество строк в таблице
- * @return номера строк, где каждый номер на новой строке
  *
+ * @param rows количество строк в таблице
  * @author Dmitry Rakovets
  * @version 1.0
+ * @return номера строк, где каждый номер на новой строке
  */
 public class Task01 {
 	public static void main(String[] args) {
@@ -17,10 +17,14 @@ public class Task01 {
 		Scanner scanner = new Scanner(System.in);
 
 		// Код необходимый для тестирования, не изменять
-		int rows = (args.length!=1) ? scanner.nextInt():Integer.parseInt(args[0]);
+		int rows = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
 
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		for (int i = 1; i < rows; i++) {
+			System.out.println(i);
+		}
+		System.out.print(rows);
 	}
 }
