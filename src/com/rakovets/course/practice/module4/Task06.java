86d8a57391adb9a417507a3d6f9b422891da0f35
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-03 18:49:10 +0300

    task: done Task06 in loop

--- src/com/rakovets/course/practice/module4/Task06.java
+++ src/com/rakovets/course/practice/module4/Task06.java
@@ -3,14 +3,14 @@ package com.rakovets.course.practice.module4;
 import java.util.Scanner;
 
 /**
- * Разработать программу для бухгалтерии:
- * Конвертировать чесловую сумму в сумму бухгалтерском формате, т.е. начиная справа, каждые три позиции отделяются
- * пробелом. Известно:
- * @param amount сумма
- * @return сумма в бухгалтерском формате
+ * Разработать программу для бухгалтерии.
+ * Конвертировать числовую сумму в сумму в бухгалтерском формате, т.е. начиная справа, каждые три позиции отделяются
+ * пробелом.
  *
+ * @param amount - сумма
  * @author Dmitry Rakovets
  * @version 1.0
+ * @return сумма в бухгалтерском формате
  */
 public class Task06 {
 	public static void main(String[] args) {
@@ -18,10 +18,20 @@ public class Task06 {
 		Scanner scanner = new Scanner(System.in);
 
 		// Код необходимый для тестирования, не изменять
-		long amount = (args.length!=1 ? scanner.nextLong():Long.parseLong(args[0]));
+		long amount = (args.length != 1 ? scanner.nextLong() : Long.parseLong(args[0]));
 
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		StringBuffer textAmount = new StringBuffer(String.valueOf(Math.abs(amount)));
+		StringBuffer reversedText = new StringBuffer(textAmount.reverse());
+		for (int i = 3; i < reversedText.length(); i += 4) {
+			reversedText.insert(i, " ");
+		}
+		if (amount < 0) {
+			System.out.print("-" + reversedText.reverse());
+		} else {
+			System.out.print(reversedText.reverse());
+		}
 	}
 }
