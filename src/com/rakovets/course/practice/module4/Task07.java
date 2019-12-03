b3dac6735adbca5c2f60c457728fb674a0698c53
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-03 18:50:08 +0300

    task: done Task07 in loop

--- src/com/rakovets/course/practice/module4/Task07.java
+++ src/com/rakovets/course/practice/module4/Task07.java
@@ -3,15 +3,15 @@ package com.rakovets.course.practice.module4;
 import java.util.Scanner;
 
 /**
- * Разработать программу для фитнес браслета:
+ * Разработать программу для фитнес браслета.
  * Программа позволяет расчитать пробег, который необходимо совершить спортсмену для подготовки к марафону. Известно:
- * @param startDistance дистанция которую пробегает спортсмен до начала тренировки
- * @param finishDistance дистанция которую желает пробежать спортсмен после окончания тренировок
- * @param dailyProgress ежедневный прогресс % по одношению к предыдущему забегу
- * @return пробег (точность 10 метром)
  *
+ * @param startDistance - дистанция которую пробегает спортсмен до начала тренировки
+ * @param finishDistance - дистанция которую желает пробежать спортсмен после окончания тренировок
+ * @param dailyProgress - ежедневный прогресс % по одношению к предыдущему забегу
  * @author Dmitry Rakovets
  * @version 1.0
+ * @return пробег (точность 10 метром)
  */
 public class Task07 {
 	public static void main(String[] args) {
@@ -19,12 +19,22 @@ public class Task07 {
 		Scanner scanner = new Scanner(System.in);
 
 		// Код необходимый для тестирования, не изменять
-		int startDistance = (args.length!=3) ? scanner.nextInt():Integer.parseInt(args[0]);
-		int finishDistance = (args.length!=3) ? scanner.nextInt():Integer.parseInt(args[1]);
-		double dailyProgress = (args.length!=3) ? scanner.nextDouble():Double.parseDouble(args[2]);
+		int startDistance = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[0]);
+		int finishDistance = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[1]);
+		double dailyProgress = (args.length != 3) ? scanner.nextDouble() : Double.parseDouble(args[2]);
 
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		double distanceAfterProgress = startDistance;
+		double distance = 0;
+		while (distanceAfterProgress < finishDistance) {
+			if (startDistance == 0 || dailyProgress == 0) {
+				break;
+			}
+			distance += distanceAfterProgress + distanceAfterProgress * dailyProgress / 100;
+			distanceAfterProgress += distanceAfterProgress * dailyProgress / 100;
+		}
+		System.out.println(distance - startDistance);
 	}
 }
