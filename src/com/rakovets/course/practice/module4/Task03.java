2c53e863ac04f7fa0b1328534dd2808341c7602e
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-03 18:45:36 +0300

    task: done Task03 in loop

--- src/com/rakovets/course/practice/module4/Task03.java
+++ src/com/rakovets/course/practice/module4/Task03.java
@@ -3,15 +3,15 @@ package com.rakovets.course.practice.module4;
 import java.util.Scanner;
 
 /**
- * Разработать программу для банка:
+ * Разработать программу для банка.
  * Необходимо расчитать прибыль, которую получит клиент по вкладу с ежегодным перерасчетом. Известно:
- * @param depositAmount сумма вклада
- * @param annualDepositPercent ежегодный процент вклада
- * @param depositTerm продолжительность вклада (в годах)
- * @return прибыль округленную математически
  *
+ * @param depositAmount - сумма вклада
+ * @param annualDepositPercent - ежегодный процент вклада
+ * @param depositTerm - продолжительность вклада (в годах)
  * @author Dmitry Rakovets
  * @version 1.0
+ * @return прибыль округленную математически
  */
 public class Task03 {
 	public static void main(String[] args) {
@@ -19,12 +19,18 @@ public class Task03 {
 		Scanner scanner = new Scanner(System.in);
 
 		// Код необходимый для тестирования, не изменять
-		double depositAmount = (args.length!=3 ? scanner.nextDouble():Double.parseDouble(args[0]));
-		double annualDepositPercent = (args.length!=3 ? scanner.nextDouble():Double.parseDouble(args[1]));
-		int depositTerm = (args.length!=3 ? scanner.nextInt():Integer.parseInt(args[2]));
+		double depositAmount = (args.length != 3 ? scanner.nextDouble() : Double.parseDouble(args[0]));
+		double annualDepositPercent = (args.length != 3 ? scanner.nextDouble() : Double.parseDouble(args[1]));
+		int depositTerm = (args.length != 3 ? scanner.nextInt() : Integer.parseInt(args[2]));
 
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		double profit = 0;
+		for (int i = 0; i < depositTerm; i++) {
+			profit += depositAmount * annualDepositPercent / 100;
+			depositAmount += depositAmount * annualDepositPercent / 100;
+		}
+		System.out.println(Math.round(depositAmount * 100.0) / 100.0);
 	}
 }
