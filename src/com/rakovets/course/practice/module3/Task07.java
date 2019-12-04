e1d58308dd80f3862e664ffbb939445794584374
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-04 22:53:10 +0300

    task: done Task07 in if-statement

--- src/com/rakovets/course/practice/module3/Task07.java
+++ src/com/rakovets/course/practice/module3/Task07.java
@@ -3,7 +3,7 @@ package com.rakovets.course.practice.module3;
 import java.util.Scanner;
 
 /**
- * Разработать программу для desktop приложения:
+ * Разработать программу для desktop приложения.
  * Определить количество дней в году.
  * В високосном годе - 366 дней, тогда как в обычном 365. Високосными годами являются все года
  * делящиеся без остатка на 4 за исключением годов, которые делятся нацело на 400
@@ -14,16 +14,21 @@ import java.util.Scanner;
  */
 
 public class Task07 {
-    public static void main(String[] args) {
-        //FIXME
-        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+	public static void main(String[] args) {
+		//FIXME
+		// Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
 
-        // Код необходимый для тестирования, не изменять
-        Scanner scanner = new Scanner(System.in);
-        int year = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
+		// Код необходимый для тестирования, не изменять
+		Scanner scanner = new Scanner(System.in);
+		int year = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
 
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-    }
+		//TODO
+		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		if (year % 400 != 0 && year % 4 == 0) {
+			System.out.println("leap year");
+		} else {
+			System.out.println("not a leap year");
+		}
+	}
 }
