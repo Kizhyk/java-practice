263fb02eea4d56eb0bc3d701413b7c3c85f147e2
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-04 22:53:28 +0300

    task: done Task09 in if-statement

--- src/com/rakovets/course/practice/module3/Task09.java
+++ src/com/rakovets/course/practice/module3/Task09.java
@@ -3,27 +3,31 @@ package com.rakovets.course.practice.module3;
 import java.util.Scanner;
 
 /**
- * Разработать программу для почты:
+ * Разработать программу для почты.
  * Определить номер подъезда дома и этаж для данной квартиры, если известно:
  *
- * @param numberFloors             - количество этажей
+ * @param numberFloors - количество этажей
  * @param numberApartmentsPerFloor - количество квартир на этаже
- * @param apartmentNumber          - номер квартиры
+ * @param apartmentNumber - номер квартиры
  * @return 'Porch: {0}. Floor: {1}', где {0} - номер подъезда, {1} - номер этажа
  */
 public class Task09 {
-    public static void main(String[] args) {
-        //FIXME
-        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+	public static void main(String[] args) {
+		//FIXME
+		// Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
 
-        // Код необходимый для тестирования, не изменять
-        Scanner scanner = new Scanner(System.in);
-        int numberFloors = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[0]);
-        int numberApartmentsPerFloor = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[1]);
-        int apartmentNumber = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[2]);
+		// Код необходимый для тестирования, не изменять
+		Scanner scanner = new Scanner(System.in);
+		int numberFloors = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[0]);
+		int numberApartmentsPerFloor = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[1]);
+		int apartmentNumber = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[2]);
 
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-    }
+		//TODO
+		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		int porchApartmentsNumber = numberFloors * numberApartmentsPerFloor;
+		int porchNumber = (int) Math.ceil((double) apartmentNumber / porchApartmentsNumber);
+		int floorNumber = (int) Math.ceil((double) (apartmentNumber - porchApartmentsNumber * (porchNumber - 1)) / numberApartmentsPerFloor);
+		System.out.println("Porch: " + porchNumber + ". Floor: " + floorNumber);
+	}
 }
