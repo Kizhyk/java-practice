7521933a2f1669386e6be3fa292e43e840c93214
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-04 22:52:27 +0300

    task: done Task02 in if-statement

--- src/com/rakovets/course/practice/module3/Task02.java
+++ src/com/rakovets/course/practice/module3/Task02.java
@@ -3,7 +3,7 @@ package com.rakovets.course.practice.module3;
 import java.util.Scanner;
 
 /**
- * Разработать программу для desktop приложения:
+ * Разработать программу для desktop приложения,
  * которая выведет приветствии для пользователя в зависимости от времени суток.
  * Известно:
  *
@@ -11,16 +11,26 @@ import java.util.Scanner;
  * @return приветствие согласно шаблону 'Good morning/day/evening/night'
  */
 public class Task02 {
-    public static void main(String[] args) {
-        //FIXME
-        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+	public static void main(String[] args) {
+		//FIXME
+		// Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
 
-        // Код необходимый для тестирования, не изменять
-        Scanner scanner = new Scanner(System.in);
-        int hour = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
+		// Код необходимый для тестирования, не изменять
+		Scanner scanner = new Scanner(System.in);
+		int hour = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
 
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-    }
+		//TODO
+		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+
+		if (hour >= 0 && hour < 6) {
+			System.out.println("Good night");
+		} else if (hour >= 6 && hour < 12) {
+			System.out.println("Good morning");
+		} else if (hour >= 12 && hour < 18) {
+			System.out.println("Good day");
+		} else {
+			System.out.println("Good evening");
+		}
+	}
 }
