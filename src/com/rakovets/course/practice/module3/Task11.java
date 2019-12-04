73d2464142109a67bf275539c55d71a3f71559c5
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-12-04 22:53:44 +0300

    task: done Task11 in if-statement

--- src/com/rakovets/course/practice/module3/Task11.java
+++ src/com/rakovets/course/practice/module3/Task11.java
@@ -3,30 +3,44 @@ package com.rakovets.course.practice.module3;
 import java.util.Scanner;
 
 /**
- * Разработать программу для desktop приложения:
+ * Разработать программу для desktop приложения.
  * Необходимо определить, какие символы вводит пользователь:
  * - латиници
  * - кирилица
  * - цифра
  * - спец. символ
  * Так же программа не должна зависеть от регистра. Известно:
+ *
  * @param letter - символ введенный пользователем
  * @return 'latin'/'cyrillic'/'digit'/'undefined'
- *
  * Help: Unicode (decimal)
  */
 
 public class Task11 {
-    public static void main(String[] args) {
-        //FIXME
-        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+	public static void main(String[] args) {
+		//FIXME
+		// Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
 
-        // Код необходимый для тестирования, не изменять
-        Scanner scanner = new Scanner(System.in);
-        char letter = (args.length != 1) ? scanner.next().charAt(0) : args[0].charAt(0);
+		// Код необходимый для тестирования, не изменять
+		Scanner scanner = new Scanner(System.in);
+		char letter = (args.length != 1) ? scanner.next().charAt(0) : args[0].charAt(0);
 
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-    }
+		//TODO
+		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		String type = "";
+		String letterString = String.valueOf(letter);
+
+		if (letterString.matches("[0-9]")) {
+            type = "digit";
+        } else if (letterString.matches("\\w")) {
+            type = "latin";
+        } else if (letterString.matches("[а-яА-Я]")) {
+            type = "cyrillic";
+        } else {
+            type = "undefined";
+        }
+
+		System.out.println(type);
+	}
 }
